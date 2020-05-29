6235dc27bd54268dab3d877f31be76d5c78f19c7
Jay Chan <jay@jay.vg>
2020-05-29 06:18:51 -0700

    Adding more words.

---
+++ gen_sixes.py
@@ -0,0 +1,23 @@
+import re
+
+fh = open('twl.txt', 'r')
+ALL_WORDS = set()
+for line in fh:
+    line = line.strip()
+    ALL_WORDS.add(line)
+
+fh.close()
+
+N = 2000
+SIXES = []
+fh = open('count_1w.txt', 'r')
+for line in fh:
+    word, _ = line.split()
+    word = word.upper()
+    if re.match(r'^[A-Z]{6}$', word) and word in ALL_WORDS:
+        SIXES.append(word)
+    if len(SIXES) >= N:
+        break
+
+for six in sorted(SIXES):
+    print six
